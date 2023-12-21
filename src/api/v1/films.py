d926d0321e24d64091651608516ef03641ca2aca
Sergey Radyukhin <22dollars@mail.ru>
2023-12-21 11:31:40 +0300

    Initial

---
+++ src/api/v1/films.py
@@ -0,0 +1,31 @@
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+
+from services.film import FilmService, get_film_service
+
+
+router = APIRouter()
+
+
+class Film(BaseModel):
+    id: str
+    title: str
+
+
+@router.get(
+    "/{film_id}",
+    response_model=Film,
+    summary="Film's search",
+    description="Fulltext film's search",
+    response_description="Film's name and rating",
+    tags=["Fulltext search"],
+)
+async def film_details(
+    film_id: str, film_service: FilmService = Depends(get_film_service)
+) -> Film:
+    film = await film_service.get_by_id(film_id)
+    if not film:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
+    return Film(id="some_id", title="some_title")
